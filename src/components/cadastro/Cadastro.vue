aadf100464ca60aed9f946f1464eb2bd790c6f87
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-28 09:48:26 -0300

    chore: v-if e v-else

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1 class="centralizado">Cadastro</h1>
-    <h2 class="centralizado">{{ foto.titulo }}</h2>
+    <h2 v-if="foto._id" class="centralizado">Alterando</h2>
+    <h2 v-else class="centralizado">Incluindo</h2>
 
     <form @submit.prevent="grava()">
       <div class="controle">
--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -1,35 +1,28 @@
-import Vue from 'vue';
+import Vue from "vue";
 
-Vue.directive('meu-transform', {
+Vue.directive("meu-transform", {
+  bind(el, binding, vnode) {
+    let current = 0;
 
-    bind(el, binding, vnode) {
+    el.addEventListener("click", function () {
+      let incremento = binding.value || 90;
+      let efeito;
 
-        let current = 0;
+      if (!binding.arg || binding.arg == "rotate") {
+        if (binding.modifiers.reverse) {
+          current -= incremento;
+        } else {
+          current += incremento;
+        }
 
-        el.addEventListener('dblclick', function() {
+        efeito = `rotate(${current}deg)`;
+      } else if (binding.arg == "scale") {
+        efeito = `scale(${incremento})`;
+      }
 
-            let incremento = binding.value || 90;
-            let efeito;
+      el.style.transform = efeito;
 
-            if(!binding.arg || binding.arg == 'rotate') {
-                
-                if(binding.modifiers.reverse) {
-                    current-=incremento;
-                } else {
-                    current+=incremento;
-                }
-
-                efeito = `rotate(${current}deg)`;
-                
-            } else if(binding.arg == "scale") {
-                efeito = `scale(${incremento})`;
-            }
-
-            el.style.transform = efeito;
-
-            if(binding.modifiers.animate) el.style.transition = 'transform 0.5s';
-        });
-    }
-
+      if (binding.modifiers.animate) el.style.transition = "transform 0.5s";
+    });
+  },
 });
-
